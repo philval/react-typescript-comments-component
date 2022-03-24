22bbad359be365477af91f68610d0b8bb59908bb
philval <philip.valentino@mac.com>
2022-03-24 20:32:12 +0000

    Update CommentsInterface to comments: IComment[];

--- src/components/CommentsInterface.ts
+++ src/components/CommentsInterface.ts
@@ -1,6 +1,6 @@
 interface IComments {
   readonly currentUser: IUser;
-  comments: Comment[];
+  comments: IComment[];
 }
 
 interface IComment {
@@ -23,7 +23,7 @@ interface IImage {
   webp: string;
 }
 
-export type { IComments, IComment, IUser, IImage };
+export type { IComments, IComment };
 
 // https://stackoverflow.com/questions/37263357/how-to-declare-and-import-typescript-interfaces-in-a-separate-file/60343829
 
