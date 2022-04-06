d07cfdff377585f0d958ecded44779cf2259c6e9
philval <philip.valentino@mac.com>
2022-04-06 22:24:07 +0100

    test rendering single <Comment /> and toggling <ReplyForm />

--- src/components/comments.test.js
+++ src/components/comments.test.js
@@ -1,16 +1,66 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-
 import App from "../App";
+import Comment from "./Comment";
 
 describe("App", () => {
   test("renders App title", () => {
     render(<App />);
 
-    expect(screen.getByRole("heading")).toBeInTheDocument;
+    expect(screen.getByRole("heading", { level: 1 })).toBeInTheDocument;
 
-    expect(screen.getByRole("heading")).toHaveTextContent(
+    expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
       "React Typescript Comments Component"
     );
+  });
+});
+
+describe("Single Comment", () => {
+  //props
+  const comment = {
+    id: 1,
+    content: "This is the comment content",
+    createdAt: "5 months ago",
+    score: 42,
+    user: {
+      image: {
+        png: "./images/avatars/image-amyrobson.png",
+        webp: "./images/avatars/image-amyrobson.webp"
+      },
+      username: "johndoe"
+    },
+    replies: []
+  };
+
+  test("renders Comment props", () => {
+    render(<Comment comment={comment} />);
+
+    expect(screen.getByText("This is the comment content")).toBeInTheDocument;
+    expect(screen.getByText("5 months ago")).toBeInTheDocument;
+    expect(screen.getByText(/^42$/)).toBeInTheDocument;
+    expect(screen.getByText("johndoe")).toBeInTheDocument;
+    expect(screen.getByRole("button", { name: "Reply" })).toHaveTextContent(
+      "Reply"
+    );
+  });
+
+  test("toggles Comment Reply form", () => {
+    render(<Comment comment={comment} />);
+
+    // when asserting that an element isn't there, use queryBy
+    expect(screen.queryByRole("textbox")).toBeNull();
+    expect(screen.queryByRole("button", { name: "REPLY" })).toBeNull();
+
+    // toggle on
+    fireEvent.click(screen.getByRole("button", { name: "Reply" }));
+    expect(screen.getByRole("textbox")).toBeInTheDocument;
+    expect(screen.getByRole("button", { name: "REPLY" })).toHaveTextContent(
+      "REPLY"
+    );
+
+    // toggle off
+    fireEvent.click(screen.getByRole("button", { name: "Reply" }));
+    expect(screen.queryByRole("textbox")).toBeNull();
+    expect(screen.queryByRole("button", { name: "REPLY" })).toBeNull();
   });
 });
