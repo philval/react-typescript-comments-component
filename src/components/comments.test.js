8c20b32c0214941f27d027cb2f3831b304bdafc2
philval <philip.valentino@mac.com>
2022-04-12 19:05:13 +0100

    Remove unused import

--- src/components/comments.test.js
+++ src/components/comments.test.js
@@ -1,9 +1,4 @@
-import {
-  render,
-  screen,
-  fireEvent,
-  getByAltText
-} from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "../App";
 import Comment from "./Comment";
