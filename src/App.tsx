8dffef4b7f7dd3d0ae32ce620e8f2ceab3572198
Philip Valentino <2657756+philval@users.noreply.github.com>
2024-01-19 15:47:18 +0000

    Add container div to <App />

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,10 @@ import Comments from "./components/Comments";
 export default function App(): JSX.Element {
   return (
     <div className="App">
-      <h1>React Typescript Comments Component</h1>
-      <Comments />
+      <div className="app-container">
+        <h1>React Typescript Comments Component</h1>
+        <Comments />
+      </div>
     </div>
   );
 }
