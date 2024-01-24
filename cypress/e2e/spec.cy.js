9745fd726f9cf9b07caf651264a92bb3e2d1a498
Philip Valentino <2657756+philval@users.noreply.github.com>
2024-01-24 18:18:14 +0000

    Refactor Cypress test suite. Delete replies to replies tests.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,14 @@
-/* eslint-disable no-undef */
+beforeEach(() => {
+  cy.intercept("http://localhost:3000/data.json", {
+    fixture: "data.json"
+  }).as("getData");
+
+  cy.visit("http://localhost:3000/");
+  cy.wait("@getData");
+});
+
 describe("Loads the application", () => {
-  it("finds the app title and first comment and new comment form", () => {
-    cy.intercept("http://localhost:3000/data.json", {
-      fixture: "data.json"
-    }).as("getData");
-
-    cy.visit("http://localhost:3000/");
-    cy.wait("@getData");
-
+  it("Finds the app title and first comment and new comment form", () => {
     cy.contains("React Typescript Comments Component");
     cy.contains("commenter1");
     cy.contains("Comment 1");
@@ -20,15 +21,7 @@ describe("Loads the application", () => {
 });
 
 describe("Adds a new comment for the user", () => {
-  it("adds a new comment", () => {
-    cy.intercept("http://localhost:3000/data.json", {
-      fixture: "data.json"
-    }).as("getData");
-
-    cy.visit("http://localhost:3000/");
-    cy.wait("@getData"); // Wait for the intercepted request to complete
-
-    cy.contains("Comment 1");
+  it("Adds a new comment for the user", () => {
     cy.get('[data-cy="inputNewComment"]').type("New comment");
     cy.get('[data-cy="submitNewComment"]').click();
     cy.contains("New comment");
@@ -36,47 +29,17 @@ describe("Adds a new comment for the user", () => {
 });
 
 describe("Replies to an existing comment", () => {
-  it("replies to replies...", () => {
-    cy.intercept("http://localhost:3000/data.json", {
-      fixture: "data.json"
-    }).as("getData");
-
-    cy.visit("http://localhost:3000/");
-    cy.wait("@getData"); // Wait for the intercepted request to complete
-
-    cy.contains("Comment 1");
+  it("Replies to an existing comment", () => {
     cy.get('[data-cy="toggleReply-1"]').click();
     cy.get("[data-cy=inputReply-1]").type("Reply One");
     cy.get('[data-cy="submitReply-1"]').click();
     cy.contains("Reply One");
-
-    cy.get('[data-cy="toggleReply-2"]').click();
-    cy.get("[data-cy=inputReply-2]").type("Reply Two");
-    cy.get('[data-cy="submitReply-2"]').click();
-    cy.contains("Reply Two");
-
-    cy.get('[data-cy="toggleReply-3"]').click();
-    cy.get("[data-cy=inputReply-3]").type("Reply Three");
-    cy.get('[data-cy="submitReply-3"]').click();
-    cy.contains("Reply Three");
-
-    cy.get('[data-cy="toggleReply-2"]').click();
-    cy.get("[data-cy=inputReply-2]").type("Reply Four");
-    cy.get('[data-cy="submitReply-2"]').click();
-    cy.contains("Reply Four");
   });
 });
 
 describe("Deletes New comments and replies", () => {
-  it("deletes a new comment", () => {
-    cy.intercept("http://localhost:3000/data.json", {
-      fixture: "data.json"
-    }).as("getData");
-
-    cy.visit("http://localhost:3000/");
-    cy.wait("@getData");
-
-    // setup add a new comment to delete
+  it("Deletes a new comment", () => {
+    // setup a new comment to delete
     cy.get('[data-cy="inputNewComment"]').type("Delete this comment");
     cy.get('[data-cy="submitNewComment"]').click();
 
@@ -84,15 +47,8 @@ describe("Deletes New comments and replies", () => {
     cy.contains("Delete this comment").should("not.exist");
   });
 
-  it("deletes a new reply", () => {
-    cy.intercept("http://localhost:3000/data.json", {
-      fixture: "data.json"
-    }).as("getData");
-
-    cy.visit("http://localhost:3000/");
-    cy.wait("@getData");
-
-    // setup add a new reply to delete
+  it("Deletes a new reply", () => {
+    // setup a new reply to delete
     cy.get('[data-cy="toggleReply-1"]').click();
     cy.get("[data-cy=inputReply-1]").type("Delete this reply");
     cy.get('[data-cy="submitReply-1"]').click();
