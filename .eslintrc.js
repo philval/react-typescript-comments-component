03b2e40b91658d60a92e77a72d2068e4d07bef7b
Philip Valentino <2657756+philval@users.noreply.github.com>
2024-05-08 19:41:09 +0000

    Update ESLint. add config notes

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,36 +28,29 @@ module.exports = {
 /*
 copilot /explain
 
-The module.exports statement is used to export the configuration object so that ESLint
-can use it.
+
 
 The env property is used to specify the environments your code is designed to run in.
-Each environment brings with it a certain set of predefined global variables. In this
-case, node: true means that the code is expected to run in a Node.js environment,
-es6: true enables ES6 (ES2015) syntax, and jest: true indicates that the code will be
-tested with Jest, a JavaScript testing framework.
+
+Each environment brings with it a certain set of predefined global variables.
+
+In this case, node: true means that the code is expected to run in a Node.js environment, es6: true enables ES6 (ES2015) syntax, and jest: true indicates that the code will be tested with Jest, a JavaScript testing framework.
 
 The extends property is a list of configurations that this configuration extends.
-These configurations can set a baseline of rules for ESLint to apply. Here, it extends
-the recommended configurations for ESLint, React, and TypeScript, as well as the
-react/jsx-runtime configuration.
 
-The parser property is set to @typescript-eslint/parser, which means that ESLint will
-use this parser to parse the TypeScript code.
+These configurations can set a baseline of rules for ESLint to apply. Here, it extends the recommended configurations for ESLint, React, and TypeScript, as well as the react/jsx-runtime configuration.
 
-The parserOptions property is an object that can contain any options that you want to
-pass to the parser. Here, it's set to recognize ECMAScript 6 and JSX syntax, and to
-treat the code as ECMAScript modules.
+The parser property is set to @typescript-eslint/parser, which means that ESLint will use this parser to parse the TypeScript code.
+
+The parserOptions property is an object that can contain any options that you want to pass to the parser. Here, it's set to recognize ECMAScript 6 and JSX syntax, and to treat the code as ECMAScript modules.
 
 The plugins property is an array of the plugins that you want to use with ESLint.
-Plugins often provide additional rules. Here, it's using the React and TypeScript ESLint
-plugins.
 
-The rules property is an object where you can override the rules specified in the
-extended configurations or define your own. It's currently empty, which means no
-additional rules are being defined or overridden.
+Plugins often provide additional rules. Here, it's using the React and TypeScript ESLint plugins.
+
+The rules property is an object where you can override the rules specified in the extended configurations or define your own. It's currently empty, which means no additional rules are being defined or overridden.
 
 Finally, the globals property is used to define global variables that are read-only.
-In this case, cy: true is defining cy as a global variable, which is commonly used in
-Cypress, an end-to-end testing framework.
+
+In this case, cy: true is defining cy as a global variable, which is commonly used in Cypress, an end-to-end testing framework.
 */
